ad47242c1cea6b6a560b449b6c3d67f093cb1350
hemg2 <hemg2@naver.com>
2022-12-27 23:51:02 +0900

    Initial Commit

---
+++ Assets/AssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetsApp.swift
+//  Assets
+//
+//  Created by 1 on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct AssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
