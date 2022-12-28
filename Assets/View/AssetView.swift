be65ba59108a6435c22bcbfb565348fa82972c35
hemg2 <hemg2@naver.com>
2022-12-28 13:25:22 +0900

    AssetView 상단뷰

---
+++ Assets/View/AssetView.swift
@@ -0,0 +1,29 @@
+//
+//  AssetView.swift
+//  Assets
+//
+//  Created by 1 on 2022/12/28.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
